987302dc8d9aa72780544e709116a8287ecea198
Victor-Cazal <vctorcazal59@gmail.com>
2024-09-10 15:35:20 +0200

    Intégration du poid et du diabète dans le formulaire

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { GlycemieComponent } from '../glycemie/glycemie.component';
+import { PoidComponent } from '../poid/poid.component';
+import { DiabeteTypeComponent } from '../diabete-type/diabete-type.component';
 
 @Component({
   selector: 'app-formulaire',
   standalone: true,
-  imports: [GlycemieComponent],
+  imports: [GlycemieComponent, PoidComponent, DiabeteTypeComponent],
   templateUrl: './formulaire.component.html',
   styleUrl: './formulaire.component.css'
 })
