f191f88ebb7e8c8999dac1cac411d54f7eae478b
antonin magner <antonin.magner@etud.univ-jfc.fr>
2024-09-10 14:36:16 +0200

    push test antonin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PatientComponent } from './patient/patient.component'; // Import du composant patient
-import { FormsModule } from '@angular/forms';
 import { DiabeteTypeComponent } from './diabete-type/diabete-type.component';
 import { AuthentificationComponent } from './authentification/authentification.component'; // Import du composant authentification
 import { GlycemieComponent } from './glycemie/glycemie.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PatientComponent, FormsModule, DiabeteTypeComponent, AuthentificationComponent, GlycemieComponent],
+  imports: [RouterOutlet, PatientComponent, ReactiveFormsModule, DiabeteTypeComponent, AuthentificationComponent, GlycemieComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
   
--- src/app/diabete-type/diabete-type.component.ts
+++ src/app/diabete-type/diabete-type.component.ts
@@ -1,70 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { FhirService } from '../fhir.service';
-import { fhirclient } from 'fhirclient/lib/types';
 import { catchError } from 'rxjs';
-
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-diabete-type',
   standalone: true,
   template: `
-    <div>
-      <input type="radio" id="type1" name="diabeteType" value="type1">
-      <label for="type1">Type 1</label>
-    </div>
-    <div>
-      <input type="radio" id="type2" name="diabeteType" value="type2">
-      <label for="type2">Type 2</label>
-    </div>
-    <button (click)="enregistrerType(this)">Enregistrer</button>
+    <form>
+      <div>
+        <input type="radio" id="type1" formControlName="diabeteType" value="type1">
+        <label for="type1">Type 1</label>
+      </div>
+      <div>
+        <input type="radio" id="type2" formControlName="diabeteType" value="type2">
+        <label for="type2">Type 2</label>
+      </div>
+      <button (click)="enregistrerType()">Enregistrer</button>
+    </form>
   `
 })
-export class DiabeteTypeComponent {
-  selectedType: string = ''; // Variable pour stocker le type sélectionné
+export class DiabeteTypeComponent implements OnInit {
+  diabeteTypeForm!: FormGroup;
 
-  constructor(private http: HttpClient, private FHIRServices : FhirService ) {
-    
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private fhirService: FhirService
+  ) {}
+
+  ngOnInit() {
+    this.diabeteTypeForm = this.fb.group({
+      diabeteType: ['type2'] // Valeur par défaut : 'type2'
+    });
   }
 
-  enregistrerType(diabetetype : DiabeteTypeComponent) {
-    diabeteType : DiabeteTypeComponent;
-    // Récupérer l'ID du patient
-    const patientId = '87d745c65dge6540127987i2'; 
+  enregistrerType() {
+    const patientId = '87d745c65dge6540127987i2';
+    const codeFhir = this.diabeteTypeForm.value.diabeteType === 'type1' ? 'E10' : 'E11';
 
-    // Déterminer le code FHIR en fonction du type sélectionné
-    const codeFhir = this.selectedType === 'type1' ? 'E10' : 'E11';
-    diabetetype.FHIRServices.post("api/DiagnosticReport/conclusion","type2");
-    // Construire la requête PUT pour mettre à jour la ressource Condition
-
-
-  }
-  enregistrerTypeTEST() {
-    const url = 'https://fhir.alliance4u.io/api/diagnostic-report';
-    const headers = { 'Content-Type': 'application/json' };
-    const patientId = '87d745c65dge6540127987i2'; 
-
-    const data = {
+    this.fhirService.post("api/diagnostic-report", {
       resourceType: 'diagnostic-report',
       subject: {
         reference: `patient/${patientId}`
       },
-      conclusion: this.selectedType,
+      conclusion: this.diabeteTypeForm.value.diabeteType,
       code: {
         coding: [
           {
             system: 'http://hl7.org/fhir/sid/icd-10',
-            code: this.selectedType === 'type2' ? 'E11' : 'E10'
+            code: codeFhir
           }
         ]
       }
-    };
-    this.http.post(url, data, { headers }).pipe(
-
-      catchError( error => {
+    }).pipe(
+      catchError(error => {
         console.error('Erreur lors de la création du DiagnosticReport.', error);
-        // Gérez l'erreur de manière appropriée, par exemple en affichant un message à l'utilisateur
-      }
-    );
+        return throwError(error);
+      })
+    ).subscribe(response => {
+      console.log('DiagnosticReport créé avec succès !', response);
+    });
   }
 }
