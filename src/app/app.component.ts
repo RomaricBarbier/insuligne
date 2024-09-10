ce5ac63d749330bec9b459524dee64fd3bf94038
Romaric Barbier <romaric.barbier@etud.univ-jfc.fr>
2024-09-10 14:47:16 +0200

    poidv1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { PatientComponent } from './patient/patient.component'; // Import du composant patient
 import { AuthentificationComponent } from './authentification/authentification.component'; // Import du composant authentification
 import { GlycemieComponent } from './glycemie/glycemie.component';
+import { PoidComponent } from './poid/poid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PatientComponent, AuthentificationComponent, GlycemieComponent],
+  imports: [RouterOutlet, PatientComponent, AuthentificationComponent, GlycemieComponent, PoidComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
