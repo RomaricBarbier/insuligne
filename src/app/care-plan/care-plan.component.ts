57bb7442ba17dc7aa7d5c1426d652535cc04fdb5
Victor-Cazal <vctorcazal59@gmail.com>
2024-09-11 09:00:33 +0200

    Création du comosant care-plan

--- src/app/care-plan/care-plan.component.ts
+++ src/app/care-plan/care-plan.component.ts
@@ -4,41 +4,41 @@ import { FhirService } from '../fhir.service';
 import { AuthentificationService } from '../authentification/authentification.service';
 
 @Component({
-  selector: 'app-graph-glycemie',
+  selector: 'app-care-plan',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './graph-glycemie.component.html',
-  styleUrl: './graph-glycemie.component.css'
+  templateUrl: './care-plan.component.html',
+  styleUrl: './care-plan.component.css'
 })
-export class GraphGlycemieComponent implements OnInit {
-  observations: any[] = []; // Variable pour stocker la liste des observations
-  filteredObservations: any[] = []; // Observations filtrées pour le patient
+export class CarePlanComponent implements OnInit {
+  carePlans: any[] = []; // Variable pour stocker la liste des observations
   selectedPatientId: string | null = null; //ID du patient
 
   constructor(private fhirService: FhirService, private idPatientAuthentification: AuthentificationService) {}
 
   //A l'initialisation de l'appli
   ngOnInit(): void {
-    this.getObservations(); // Appel de la méthode pour récupérer les patients au chargement
+    this.getCarePlans(); // Appel de la méthode pour récupérer les patients au chargement
     this.selectedPatientId = this.idPatientAuthentification.getSelectedPatientId();
     console.log('Selected patient ID:', this.selectedPatientId);
   }
 
-  getObservations(): void {
-    const url = 'api/observation';
+  getCarePlans(): void {
+    const url = 'api/care-plan';
 
     // Appel au service pour récupérer la liste des observations
     this.fhirService.get(url).subscribe({
       next: (data) => {
-        this.observations = data;
-        console.log('Liste des observations:', this.observations); // Afficher dans la console la liste récupérée
+        this.carePlans = data;
+        console.log('Liste des care-plans:', this.carePlans); // Afficher dans la console la liste récupérée
       },
       error: (error) => {
-        console.error('Erreur lors de la récupération des patients:', error);
+        console.error('Erreur lors de la récupération des care-plans:', error);
       },
       complete: () => {
-        console.log('Récupération de la liste des patients terminée');
+        console.log('Récupération de la liste des care-plans terminée');
       }
     });
   }
+
 }
