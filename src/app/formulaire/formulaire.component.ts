628a8cececacf4a41db15d9e4743388273907115
Romaric Barbier <romaric.barbier@etud.univ-jfc.fr>
2024-09-11 09:31:00 +0200

    symptome

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -3,11 +3,12 @@ import { GlycemieComponent } from '../glycemie/glycemie.component';
 import { PoidComponent } from '../poid/poid.component';
 import { DiabeteTypeComponent } from '../diabete-type/diabete-type.component';
 import { MedicamentationComponent } from '../medicamentation/medicamentation.component';
+import { SymptomFormComponent } from '../symptome/symptome.component';
 
 @Component({
   selector: 'app-formulaire',
   standalone: true,
-  imports: [GlycemieComponent, PoidComponent, DiabeteTypeComponent, MedicamentationComponent],
+  imports: [GlycemieComponent, PoidComponent, DiabeteTypeComponent, MedicamentationComponent, SymptomFormComponent],
   templateUrl: './formulaire.component.html',
   styleUrl: './formulaire.component.css'
 })
