87741ed2a029e2a7d4c8e3eea1992bd175d97bff
Victor-Cazal <vctorcazal59@gmail.com>
2024-09-11 08:19:47 +0200

    Ajout du patient ID dans le composant graph-glycemi

--- src/app/graph-glycemie/graph-glycemie.component.ts
+++ src/app/graph-glycemie/graph-glycemie.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FhirService } from '../fhir.service';
+import { AuthentificationService } from '../authentification/authentification.service';
 
 @Component({
   selector: 'app-graph-glycemie',
@@ -9,14 +10,17 @@ import { FhirService } from '../fhir.service';
   templateUrl: './graph-glycemie.component.html',
   styleUrl: './graph-glycemie.component.css'
 })
-export class GraphGlycemieComponent {
+export class GraphGlycemieComponent implements OnInit {
   observations: any[] = []; // Variable pour stocker la liste des observations
+  selectedPatientId: string | null = null; //ID du patient
 
-  constructor(private fhirService: FhirService) {}
+  constructor(private fhirService: FhirService, private idPatientAuthentification: AuthentificationService) {}
 
   //A l'initialisation de l'appli
   ngOnInit(): void {
     this.getObservations(); // Appel de la méthode pour récupérer les patients au chargement
+    this.selectedPatientId = this.idPatientAuthentification.getSelectedPatientId();
+    console.log('Selected patient ID:', this.selectedPatientId);
   }
 
   getObservations(): void {
@@ -26,7 +30,7 @@ export class GraphGlycemieComponent {
     this.fhirService.get(url).subscribe({
       next: (data) => {
         this.observations = data;
-        console.log('Liste des patients:', this.observations); // Afficher dans la console la liste récupérée
+        console.log('Liste des observations:', this.observations); // Afficher dans la console la liste récupérée
       },
       error: (error) => {
         console.error('Erreur lors de la récupération des patients:', error);
