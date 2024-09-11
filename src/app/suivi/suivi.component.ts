dec9f9bbacb9386b11dbcd5f63cecce8e4b2210e
Victor-Cazal <vctorcazal59@gmail.com>
2024-09-11 11:31:58 +0200

    Ajout du composant medication-request

--- src/app/suivi/suivi.component.ts
+++ src/app/suivi/suivi.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { GraphGlycemieComponent } from '../graph-glycemie/graph-glycemie.component';
 import { PractitionerComponent } from '../practitioner/practitioner.component';
 import { CarePlanComponent } from '../care-plan/care-plan.component';
+import { MedicationRequestComponent } from '../medication-request/medication-request.component';
 
 @Component({
   selector: 'app-suivi',
   standalone: true,
-  imports: [GraphGlycemieComponent, PractitionerComponent, CarePlanComponent],
+  imports: [GraphGlycemieComponent, PractitionerComponent, CarePlanComponent, MedicationRequestComponent],
   templateUrl: './suivi.component.html',
   styleUrl: './suivi.component.css'
 })
